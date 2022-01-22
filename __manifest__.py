11772eb27a6cdba58233d133b1c3025a73db3e16
Panca <ppakpahan@solusiaglis.co.id>
2022-01-22 11:56:42 +0700

    first commit

---
+++ __manifest__.py
@@ -0,0 +1,16 @@
+{
+    "name"      : "Delete Journal Items ",
+    "summary"   : "Karena terlanjur import journal entry dengan journal items yg besar yaitu lebih besar dari  30 ribu lines maka mau gak mau harus delete menggunakan module ini",
+    "version"   : "14.0.1.0.0",
+    "category"  : "Uncategorized",
+    "website"   : "https://solusiaglis.co.id",
+    "author"    : "ppakpahan@solusiaglis.co.id",
+    "license"   : "AGPL-3",
+    "application"   : False,
+    "installable" : True,
+    "external_dependencies" : {
+    },
+    "data" : [
+        "data/data.xml",
+    ],
+}
